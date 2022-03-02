472f97fdde9219d57ed8f5f82b959a45c1a41cb4
Pavel Nosov <soundrussian@gmail.com>
2022-03-02 10:04:01 +0300

    Log body content

--- api/middleware/log.go
+++ api/middleware/log.go
@@ -1,24 +1,28 @@
 package middleware
 
 import (
+	"bytes"
+	"github.com/rs/zerolog/log"
 	"github.com/soundrussian/go-practicum-diploma/pkg/logging"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
 type logEntry struct {
-	userAgent string
-	method    string
-	addr      string
-	path      string
+	body   string
+	method string
+	addr   string
+	path   string
 }
 
 func fromRequest(r *http.Request) logEntry {
+
 	le := logEntry{
-		userAgent: r.Header.Get("user-agent"),
-		method:    r.Method,
-		addr:      r.RemoteAddr,
-		path:      r.URL.Path,
+		body:   r.Header.Get("user-agent"),
+		method: r.Method,
+		addr:   r.RemoteAddr,
+		path:   r.URL.Path,
 	}
 
 	return le
@@ -28,15 +32,29 @@ func LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, logger := logging.CtxLogger(r.Context())
 
-		logEntry := fromRequest(r)
+		entry := fromRequest(r)
+
+		buf, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Error().Msgf("[%s] %s Error reading request body: %v", strings.ToUpper(entry.method), entry.path, err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		entry.body = string(buf)
+
+		// Make sure body can be read again in next handlers
+		// See https://gist.github.com/briangershon/fa9feb08e6a65d52bdc35c738d8cf104?permalink_comment_id=3719593#gistcomment-3719593
+		reader := ioutil.NopCloser(bytes.NewBuffer(buf))
+		r.Body = reader
+
 		logger = logger.With().
-			Str("method", logEntry.method).
-			Str("addr", logEntry.addr).
-			Str("path", logEntry.path).
-			Str("ua", logEntry.userAgent).
+			Str("method", entry.method).
+			Str("addr", entry.addr).
+			Str("path", entry.path).
+			Str("body", entry.body).
 			Logger()
 
-		logger.Info().Msgf("[%s] %s", strings.ToUpper(logEntry.method), logEntry.path)
+		logger.Info().Msgf("[%s] %s", strings.ToUpper(entry.method), entry.path)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
