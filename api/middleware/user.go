c01b5912a05522ff3cc0522f6100dde584200301
Pavel Nosov <soundrussian@gmail.com>
2022-03-06 09:04:49 +0300

    Fix go vet

--- api/middleware/user.go
+++ api/middleware/user.go
@@ -16,13 +16,13 @@ func CurrentUser(next http.Handler) http.Handler {
 
 		if err != nil {
 			logger.Err(err).Msg("failed to fetch token from request context")
-			http.Error(w, err.Error(), 401)
+			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
 
 		if token == nil || jwt.Validate(token) != nil {
 			logger.Error().Msg("jwt token is nil or invalid")
-			http.Error(w, http.StatusText(401), 401)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 
@@ -33,13 +33,13 @@ func CurrentUser(next http.Handler) http.Handler {
 
 		if v, ok = claims["user_id"]; !ok {
 			logger.Error().Msg("no user_id in jwt claims")
-			http.Error(w, http.StatusText(401), 401)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 
 		if userIDAsFloat, ok = v.(float64); !ok {
 			logger.Error().Msgf("could not convert %v to float64", v)
-			http.Error(w, http.StatusText(401), 401)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
 		}
 
