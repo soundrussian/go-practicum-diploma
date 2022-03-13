64a74a2874c388adae2646cb9ba71a47b0014a6b
Pavel Nosov <soundrussian@gmail.com>
2022-03-13 16:15:39 +0300

    Fix default order value

--- storage/psql/db/migrations/000003_create_orders_table.up.sql
+++ storage/psql/db/migrations/000003_create_orders_table.up.sql
@@ -2,8 +2,8 @@ BEGIN;
 CREATE TABLE orders(
     order_id varchar(255) PRIMARY KEY,
     user_id integer REFERENCES users ON DELETE CASCADE,
-    accrual integer NOT NULL DEFAULT 1,
-    status integer DEFAULT 0,
+    accrual integer NOT NULL DEFAULT 0,
+    status integer DEFAULT 1,
     uploaded_at timestamp
 );
 CREATE INDEX ON orders(user_id);
